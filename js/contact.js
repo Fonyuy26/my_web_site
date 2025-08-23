9c29ca4e0096ea87f01e7f127d0e14c387d02b91
Ajavi Jevis Fonyuy <ajavifonyuyjevis@gmail.com>
2025-08-23 10:19:07 +0100

    form

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,16 @@
-(function() {
-    emailjs.init("S_P2zGuS3NuI3fw3p"); // Replace with your EmailJS Public Key
-})();
+<script src="https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js"></script>
 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+  (function() {
+    emailjs.init("S_P2zGuS3NuI3fw3p"); 
+  })();
+
+  document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
     emailjs.sendForm("service_u26zrea", "template_rcra1ii", this)
       .then(function() {
-          alert("Message sent successfully!");
-          document.getElementById("contactForm").reset();
+        alert("Message sent successfully ✅");
       }, function(error) {
-          alert("Failed to send message. Please try again.\n" + JSON.stringify(error));
+        alert("Failed to send message ❌: " + JSON.stringify(error));
       });
-});
+  });
